847b2d889866e6f91f8a541631a20d80baf1fd58
AryanG <aryangera8@gmail.com>
2022-11-22 17:09:35 +0530

    new3.2

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,re_path
 
 from .views import register,login,authuser,authCce,authCse,authEce,authMMe,grant_Perms,logout
-from .admin_views import create_job,nextRnd,schedule,Fetch_Jobs,add_dept,add_spez,delete_job,Fetch_applications,Reject
+from .admin_views import create_job,nextRnd,schedule,Fetch_Jobs,add_dept,add_spez,delete_job,Fetch_applications,Reject,addPost,getPosts
 from .application_views import register_Application,get_details,update_Application,delete_app
 from .dev_views import all_Jobs,all_apps,all_dept,all_spez,all_Users
 from .unauth_views import FetchAllDept,FetchAllJobs,FetchAllSpez
@@ -16,6 +16,8 @@ urlpatterns = [
     path('deleteJob',delete_job),
     path('registerApp',register_Application),
     path('fetchJobs',Fetch_Jobs),
+    path('addPost',addPost),
+    path('getPosts',getPosts),
     path('schedule',schedule),
     path('nextRound',nextRnd),
     path('rg',register),
